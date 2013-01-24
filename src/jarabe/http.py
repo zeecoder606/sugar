ade030943fa516eaac6df40021f4a7b15c74728a
Daniel Narvaez <dwnarvaez@gmail.com>
2013-01-24 14:44:26 +0100

    Server html toolkit files

--- src/jarabe/http.py
+++ src/jarabe/http.py
@@ -4,17 +4,19 @@ import BaseHTTPServer
 import os
 
 from jarabe.model import bundleregistry
+from jarabe import config
 
 class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         splitted = self.path.split("/")
-        bundle_id = splitted[1]
-        path = splitted[2:]
+        if splitted[1] == "toolkit":
+            base_path = config.html_toolkit_path
+        else:
+            registry = bundleregistry.get_registry()
+            bundle = registry.get_bundle(splitted[1])
+            base_path = bundle.get_path()
 
-        registry = bundleregistry.get_registry()
-        bundle = registry.get_bundle(bundle_id)
-
-        file_path = os.path.join(bundle.get_path(), *path)
+        file_path = os.path.join(base_path, *splitted[2:])
         with open(file_path) as f:
             self.send_response(200)
             self.send_header('Content-type', mimetypes.guess_type(file_path))
