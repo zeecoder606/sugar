8ba2558e2906f7478252d980fb240f33a29f7e89
Daniel Narvaez <dwnarvaez@gmail.com>
2013-01-29 11:59:28 +0100

    Revert "Server html toolkit files"
    This reverts commit ade030943fa516eaac6df40021f4a7b15c74728a.

--- src/jarabe/http.py
+++ src/jarabe/http.py
@@ -4,19 +4,17 @@ import BaseHTTPServer
 import os
 
 from jarabe.model import bundleregistry
-from jarabe import config
 
 class HTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         splitted = self.path.split("/")
-        if splitted[1] == "toolkit":
-            base_path = config.html_toolkit_path
-        else:
-            registry = bundleregistry.get_registry()
-            bundle = registry.get_bundle(splitted[1])
-            base_path = bundle.get_path()
+        bundle_id = splitted[1]
+        path = splitted[2:]
 
-        file_path = os.path.join(base_path, *splitted[2:])
+        registry = bundleregistry.get_registry()
+        bundle = registry.get_bundle(bundle_id)
+
+        file_path = os.path.join(bundle.get_path(), *path)
         with open(file_path) as f:
             self.send_response(200)
             self.send_header('Content-type', mimetypes.guess_type(file_path))
